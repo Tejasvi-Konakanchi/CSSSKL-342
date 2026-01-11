a7bba9fbcc33bd66b7aafa0dbfc60b17a13c7bdd
Tejasvi Konakanchi <tkonak@uw.edu>
2026-01-10 23:24:16 -0800

    Working on Exercise 3!

---
+++ All-Labs/Lab1/3.File-IO/FileOperationPractice.cpp
@@ -0,0 +1,15 @@
+/*
+ *  Course: CSSSKL342A
+ *  Lab 1: Toolchains, type conversion, references, file I/O
+ *
+ *  FileOperationPractice.cpp
+ *  Author: Tejasvi Konakanchi
+ *  Date: 1/10/2025
+ *
+ */
+
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+using namespace std;
+
