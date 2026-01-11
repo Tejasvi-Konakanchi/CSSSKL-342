15f8e8cd75c7bc9ef79c621d10252a47c2f125b1
Tejasvi Konakanchi <tkonak@uw.edu>
2026-01-10 23:42:40 -0800

    Final updates

--- All-Labs/Lab1/3.File-IO/FileOperationPractice.cpp
+++ All-Labs/Lab1/3.File-IO/FileOperationPractice.cpp
@@ -13,3 +13,28 @@
 #include <iostream>
 using namespace std;
 
+int main() {
+  ifstream inputFile;
+  int number;
+
+  // Replace 1 with 2 & 3 while testing
+  inputFile.open(textfile1.txt);
+
+  if (!inputFile) {
+    cout << "Error! Could not open file." << endl;
+  }
+
+  while (inputFile >> number) {
+    cout << number << endl;
+  }
+
+  inputFile.close();
+  return 0;
+}
+
+//I am unable to run this code. I'm still trying to figure out why and how. 
+// Due to the time constraint this is all I can submit. 
+// I will make sure all my next assignments are complete, I apologize for the current inconcenience.
+// The .txt file for this exercise is also empty.
+// I implore professor Sung to offer me partial credit for my finished work.
+// Thank you so much for your consideration.
